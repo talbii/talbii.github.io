bb38cc7434c59576d73c7493c4f993f2b42ea0d0
talbii <41526934+talbii@users.noreply.github.com>
2021-06-27 10:40:52 +0300

    Updated spacing between nav links

---
+++ nav.js
@@ -0,0 +1,7 @@
+document.write('\
+<ul>\
+    <li><a class="nav-link" href="/index.html">home</a></li>\
+    <li><a class="nav-link" href="/pages/about.html">about</a></li>\
+    <li><a class="nav-link" href="/pages/contact.html">contact</a></li>\
+</ul>\
+');
